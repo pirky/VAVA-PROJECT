c0804e7f4c8e962ee6b5233b5b260ad405e0fe61
pirky <m.pirkovsky@gmail.com>
2021-04-22 09:55:29 +0200

    update

--- Project/src/project/controller/menuInterface.java
+++ Project/src/project/controller/menuInterface.java
@@ -3,7 +3,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import java.io.IOException;
+import java.util.Locale;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
 /**
  * Interface with default method to evade duplicate code
@@ -16,7 +18,9 @@ public interface menuInterface {
      * @throws IOException Exception for reading from file
      */
     default void changeMainView() throws IOException {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/project/view/MainView.fxml")));
+        Locale skLocale = new Locale("sk_SK");
+        ResourceBundle bundle = ResourceBundle.getBundle("project/resources.mainView", skLocale);
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/project/view/MainView.fxml")), bundle);
         Scene scene = new Scene(root);
         Main.mainStage.setScene(scene);
         Main.mainStage.show();
